737d862ad135764bdccdb91257939530739ffeb8
ddaniss <danielssocas@gmail.com>
2021-02-25 17:54:17 +0000

    Modify Tech and Nav heights

--- src/app/Nav/style.js
+++ src/app/Nav/style.js
@@ -6,7 +6,7 @@ export const Header = styled.nav`
   align-content: center;
 
   background-color: #000;
-  height: 105px;
+  height: 100px;
   width: 100%;
   padding: 1.5em 6em;
   /* margin-top: -100px; */
